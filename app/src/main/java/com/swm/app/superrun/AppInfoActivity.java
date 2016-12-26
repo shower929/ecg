ecad51e4ceb37304f2b4253aecd25dbff18fe1a8
shower929 <shower929@gmail.com>
2016-12-26 13:27:25 +0800

    Add app info

---
+++ app/src/main/java/com/swm/app/superrun/AppInfoActivity.java
@@ -0,0 +1,15 @@
+package com.swm.app.superrun;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.swm.heart.R;
+
+public class AppInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_app_info);
+    }
+}
