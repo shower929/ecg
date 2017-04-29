71dc71bfdd1c35eedf002aee6b134ed04d121530
Shower <shower929@gmail.com>
2017-04-29 21:29:10 +0800

    Add ble control

---
+++ demo-app/src/main/java/com/swm/demo/BleDeviceListItemViewHolder.java
@@ -0,0 +1,26 @@
+package com.swm.demo;
+
+import android.bluetooth.BluetoothDevice;
+import android.view.View;
+import android.widget.TextView;
+
+/**
+ * Created by yangzhenyu on 2016/9/22.
+ */
+
+public class BleDeviceListItemViewHolder {
+    public final TextView mDeviceName;
+    public final TextView mDeviceAddress;
+    public final View mItemLayout;
+
+    public BleDeviceListItemViewHolder(View group) {
+        mDeviceName = (TextView) group.findViewById(R.id.swm_ble_device_name);
+        mDeviceAddress = (TextView) group.findViewById(R.id.swm_ble_device_address);
+        mItemLayout = group;
+    }
+
+    public void updateView(final BluetoothDevice device) {
+        mDeviceName.setText(device.getName());
+        mDeviceAddress.setText(device.getAddress());
+    }
+}
