cfbf9982216c5b6464122015d56bb9ebe7e52d81
Shower <shower929@gmail.com>
2017-04-29 21:38:52 +0800

    Update demo app

--- demo-app/src/main/java/com/swm/demo/MainActivity.java
+++ demo-app/src/main/java/com/swm/demo/MainActivity.java
@@ -1,13 +1,74 @@
 package com.swm.demo;
 
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.swm.demo.view.CalorieView;
+import com.swm.demo.view.HeartRateView;
+import com.swm.demo.view.PhyAgeView;
+import com.swm.demo.view.StepView;
+import com.swm.demo.view.StressView;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String LOG_TAG = "Demo";
+    private SwmService.SwmBinder swmBinder;
+    private Presenter presenter;
+    private HeartRateView heartRateView;
+    private CalorieView calorieView;
+    private StressView stressView;
+    private PhyAgeView phyAgeView;
+    private StepView stepView;
+    private DeviceStatus deviceStatus;
+
+    private ServiceConnection connection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            Log.i(LOG_TAG, "Connected to SWM service");
+            swmBinder = (SwmService.SwmBinder) service;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this, SwmService.class);
+        bindService(intent, connection, BIND_AUTO_CREATE);
+
         setContentView(R.layout.activity_main);
+        heartRateView = (HeartRateView) findViewById(R.id.swm_heart_rate);
+        calorieView = (CalorieView) findViewById(R.id.swm_calorie);
+        stressView = (StressView) findViewById(R.id.swm_stress);
+        phyAgeView = (PhyAgeView) findViewById(R.id.swm_phy_age);
+        stepView = (StepView) findViewById(R.id.swm_step);
+        deviceStatus = (DeviceStatus) findViewById(R.id.ble_device_status);
+
+        presenter = new Presenter(this, heartRateView, calorieView, stressView, phyAgeView, stepView, deviceStatus);
+        getApplication().registerActivityLifecycleCallbacks(presenter);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        getApplication().unregisterActivityLifecycleCallbacks(presenter);
+        unbindService(connection);
     }
 }
---
+++ demo-app/src/main/java/com/swm/demo/Presenter.java
@@ -0,0 +1,151 @@
+package com.swm.demo;
+
+import android.app.Activity;
+import android.app.Application;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+
+import com.swm.demo.view.CalorieView;
+import com.swm.demo.view.HeartRateView;
+import com.swm.demo.view.PhyAgeView;
+import com.swm.demo.view.StepView;
+import com.swm.demo.view.StressView;
+import com.swm.sdk.CaloriePlugin;
+import com.swm.sdk.DeviceListener;
+import com.swm.sdk.HeartEngine;
+import com.swm.sdk.HrvPlugin;
+import com.swm.sdk.RunningPlugin;
+import com.swm.sdk.SwmDevice;
+
+/**
+ * Created by yangzhenyu on 2017/4/24.
+ */
+
+public class Presenter implements Application.ActivityLifecycleCallbacks{
+    private final HeartRateView heartRateView;
+    private final CalorieView calorieView;
+    private final StressView stressView;
+    private final PhyAgeView phyAgeView;
+    private final StepView stepView;
+    private final DeviceStatus deviceStatus;
+    private IntentFilter intentFilter;
+    private BroadcastReceiver broadcastReceiver;
+    private final Activity myActivity;
+    private int stepCount;
+
+    public Presenter(Activity activity, HeartRateView heartRateView, CalorieView calorieView, StressView stressView, PhyAgeView phyAgeView, StepView stepView, DeviceStatus deviceStatus) {
+        this.myActivity = activity;
+        this.heartRateView = heartRateView;
+        this.calorieView = calorieView;
+        this.stressView = stressView;
+        this.phyAgeView = phyAgeView;
+        this.stepView = stepView;
+        this.deviceStatus = deviceStatus;
+    }
+
+    private void registerBroadcastReceiver() {
+        intentFilter = new IntentFilter(HeartEngine.ACTION_HEART_RATE);
+        intentFilter.addAction(CaloriePlugin.ACTION_CALORIE_AVAILABLE);
+        intentFilter.addAction(HrvPlugin.ACTION_STRESS_CHANGED);
+        intentFilter.addAction(HrvPlugin.ACTION_PHYSICAL_AGE_CHANGED);
+        intentFilter.addAction(SwmDevice.ACTION_DEVICE_EVENT);
+        intentFilter.addAction(RunningPlugin.ACTION_STEP);
+
+        broadcastReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                if(intent.getAction().equals(HeartEngine.ACTION_HEART_RATE))
+                    handleHeartRateBroadcast(intent);
+                else if (intent.getAction().equals(CaloriePlugin.ACTION_CALORIE_AVAILABLE))
+                    handleCalorie(intent);
+                else if (intent.getAction().equals((HrvPlugin.ACTION_STRESS_CHANGED)))
+                    handleStressChange(intent);
+                else if (intent.getAction().equals(HrvPlugin.ACTION_PHYSICAL_AGE_CHANGED))
+                    handlePhyAgeChange(intent);
+                else if (intent.getAction().equals(SwmDevice.ACTION_DEVICE_EVENT))
+                    handleDeviceEvent(intent);
+                else if (intent.getAction().equals(RunningPlugin.ACTION_STEP))
+                    handleStep(intent);
+
+            }
+        };
+        myActivity.registerReceiver(broadcastReceiver, intentFilter);
+    }
+
+    private void handleCalorie(Intent intent) {
+        float calorie = intent.getFloatExtra(CaloriePlugin.EXTRA_CALORIE, 0f);
+        calorieView.setCalorie(calorie);
+    }
+
+    private void handleStressChange(Intent intent) {
+        com.swm.sdk.HrvPlugin.Stress stress = (HrvPlugin.Stress) intent.getSerializableExtra(HrvPlugin.EXTRA_STRESS);
+        stressView.setStress(stress);
+    }
+
+    private void handlePhyAgeChange(Intent intent) {
+        int phyAge = intent.getIntExtra(HrvPlugin.EXTRA_PHYSICAL_AGE, 0);
+        phyAgeView.setPhyAge(phyAge);
+    }
+
+    private void handleDeviceEvent(Intent intent) {
+        int state = intent.getIntExtra(SwmDevice.EXTRA_CONNECTION_STATUS, 0);
+        if(state == SwmDevice.CONNECTED)
+            deviceStatus.active();
+        else
+            deviceStatus.inactive();
+    }
+
+    public void handleStep(Intent intent) {
+        int step = intent.getIntExtra(RunningPlugin.EXTRA_STEP, 0);
+        stepCount += step;
+        stepView.setStep(stepCount);
+    }
+
+    private void unregisterroadcastReceiver() {
+        myActivity.unregisterReceiver(broadcastReceiver);
+    }
+
+    private void handleHeartRateBroadcast(Intent intent) {
+        heartRateView.setHeartRate(intent.getIntExtra(HeartEngine.HEART_RATE, 0));
+    }
+
+    @Override
+    public void onActivityCreated(Activity activity, Bundle savedInstanceState) {
+
+    }
+
+    @Override
+    public void onActivityStarted(Activity activity) {
+        if (activity == myActivity)
+            registerBroadcastReceiver();
+    }
+
+    @Override
+    public void onActivityResumed(Activity activity) {
+
+    }
+
+    @Override
+    public void onActivityPaused(Activity activity) {
+
+    }
+
+    @Override
+    public void onActivityStopped(Activity activity) {
+        if (activity == myActivity)
+            unregisterroadcastReceiver();
+    }
+
+    @Override
+    public void onActivitySaveInstanceState(Activity activity, Bundle outState) {
+
+    }
+
+    @Override
+    public void onActivityDestroyed(Activity activity) {
+
+    }
+}
