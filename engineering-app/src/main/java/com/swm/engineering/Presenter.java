386e127e90ad59cc9ea6626244e90861bf2955e3
Shower <shower929@gmail.com>
2017-05-08 16:01:10 +0800

    Presenter common methods

---
+++ engineering-app/src/main/java/com/swm/engineering/Presenter.java
@@ -0,0 +1,12 @@
+package com.swm.engineering;
+
+/**
+ * Created by yangzhenyu on 2017/5/3.
+ */
+
+public abstract class Presenter {
+    abstract void onStart();
+    abstract void onStop();
+    abstract void show();
+    abstract void hide();
+}
